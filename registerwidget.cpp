a6024cbe0ad688528a3ef06cf06b2614c0009c9c
hadisandid <hxa04@mail.Aub.edu>
2019-10-04 17:40:05 +0300

    Added comments in my part

--- registerwidget.cpp
+++ registerwidget.cpp
@@ -8,24 +8,40 @@ registerWidget::registerWidget(QWidget *parent) :
     // ALL CODE HERE SHALL BE CONSIDERED TEMPLATE
     // This is Hadi's Part
 
-    test_screen = new QLabel("This is the Login Page");
-    button = new QPushButton("Confirm Registration");
+    // What elements should be added to the register page?
+    // First name, Last name, username, password, date of birth
+    // Profile picture, gender
+    // Special considerations should be made for the password.
+    // QLabels needed : Welcome text, fname, lname, username, gender
+    // date of birth, password, profile picture, confirm password, Error message
 
-    test_layout = new QVBoxLayout();
+    // QLineEdits needed : fname, lname, username, password, confirm password
 
-    test_layout->addWidget(test_screen);
-    test_layout->addWidget(button);
+    // QSpinBox needed : 3 for date of birth : day,month,year
+
+    // QDateEdit : date of birth
+
+    // QRadioButtons needed : male,female,do not specify
+    // + don't forget the vboxlayout.
+
+    // QPushButtons needed :  Cancel, Confirm, select picture
+
+    // Challenges :
+
+    //Creating a cadre for the picture to be displayed in the register page ( with a default blank pic)
+    //copy the picture selected by the user in the working directory
+    //Need to address the "save to JSON" issue to add separate users
+    //Maybe apply a specific style ?
+
+    grid_01 = new QVBoxLayout();
 
 
-    this->setLayout(test_layout);
 
-    QObject::connect(button,SIGNAL(clicked()),this,SLOT(GoToLoginPage()));
+
+    this->setLayout(grid_01);
+
+
 }
 
 
-void registerWidget::GoToLoginPage()
-{
-loginWidget *loginInit = new loginWidget();
-loginInit->show();
-this->close();
-}
+
--- registerwidget.h
+++ registerwidget.h
@@ -10,14 +10,41 @@ class registerWidget : public QWidget
 public:
     explicit registerWidget(QWidget *parent = 0);
 
-    QLabel *test_screen;
-    QPushButton *button;
-    QVBoxLayout *test_layout;
+    QGridLayout *grid_01;
+    QVBoxLayout *vbox_radio;
+
+    QLabel *welcome_text;
+    QLabel *fname_text;
+    QLabel *lname_text;
+    QLabel *username_text;
+    QLabel *gender_text;
+    QLabel *date_of_birth_text;
+    QLabel *password_text;
+    QLabel *password_confirm_text;
+    QLabel *profile_picture_text;
+    QLabel *error_message_text;
+
+    QLineEdit *fname_edit;
+    QLineEdit *lname_edit;
+    QLineEdit *username_edit;
+    QLineEdit *password_edit;
+    QLineEdit *password_confirm_edit;
+
+    QDateEdit *date_of_birth_date_edit;
+
+    QRadioButton *male_radio_button;
+    QRadioButton *female_radio_button;
+
+    QPushButton *select_picture_button;
+    QPushButton *cancel_button;
+    QPushButton *confirm_button;
+
+
 
 signals:
 
 public slots:
-    void GoToLoginPage();
+
 
 
 };
