21990fb4b3e6a531afd270bcda3e94d442f46471
hadisandid <hxa04@mail.Aub.edu>
2019-10-05 21:42:32 +0300

    Added some comments

--- registerwidget.cpp
+++ registerwidget.cpp
@@ -6,6 +6,32 @@ registerWidget::registerWidget(QWidget *parent) :
     QWidget(parent)
 {
 
+    // Add title to page
+        // What elements should be added to the register page?
+        // First name, Last name, username, password, date of birth
+        // Profile picture, gender
+        // Special considerations should be made for the password.
+        // QLabels needed : Welcome text, fname, lname, username, gender
+        // date of birth, password, profile picture, confirm password, Error message
+
+        // QLineEdits needed : fname, lname, username, password, confirm password
+
+        // QSpinBox needed : 3 for date of birth : day,month,year
+
+        // QDateEdit : date of birth
+
+        // QRadioButtons needed : male,female,do not specify
+        // + don't forget the vboxlayout.
+
+        // QPushButtons needed :  Cancel, Confirm, select picture
+
+        // Challenges :
+
+        //Creating a cadre for the picture to be displayed in the register page ( with a default blank pic)
+        //copy the picture selected by the user in the working directory
+        //Need to address the "save to JSON" issue to add separate users
+        //Maybe apply a specific style ?
+
     welcome = new QLabel("Please fill the following to register your account");
     fname = new QLabel("First name");
     fname_edit = new QLineEdit();
